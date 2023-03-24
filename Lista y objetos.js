9707c72bb99d52c4071fcb1fbdca0550a4bcf832
brian gur <brianguridi2013@gmail.com>
2023-03-24 20:44:18 -0300

    javascript list

---
+++ Lista y objetos.js
@@ -0,0 +1,8 @@
+const lista = ["Brian" , 31 , false , "26/05/1991" , ];
+const libro = {
+    Titulo : "Cementerio de animales",
+    Autor : "Stephen king",
+    Fecha : 1983,
+}
+console.log(lista)
+console.log(libro)
